c8858a5fc6ee45bb11a7ca96c41c0d5c997b3d84
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2025-07-08 02:34:29 +0530

    Loading state update

--- frontend/src/pages/blog/myblogs/MyBlogs.jsx
+++ frontend/src/pages/blog/myblogs/MyBlogs.jsx
@@ -6,7 +6,7 @@ import BlogCard from "../home/components/BlogCard";
 import notebook from "../../../assets/Notebook-bro.svg";
 
 const MyBlogs = () => {
-  const { getMyBlogs, myBlogs } = useBlogStore();
+  const { getMyBlogs, myBlogs, isBlogsLoading } = useBlogStore();
 
   const navigate = useNavigate();
 
@@ -23,7 +23,7 @@ const MyBlogs = () => {
       <div className="px-14 space-y-5 fixed m-10 lg:flex hidden flex-col items-center">
         <img src={notebook} className="size-52 object-cover" alt="welcome" />
       </div>
-      {myBlogs.length === 0 ? (
+      {!isBlogsLoading && myBlogs.length === 0 ? (
         <div className="max-w-screen-xl mx-auto flex flex-col lg:flex-row py-20 px-10">
           <div className="flex-1 lg:flex gap-4 items-center justify-center">
             <img src={createBlogImg} className="max-h-96" alt="createBlog" />
