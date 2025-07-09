58d66a0648fb8a5e66dfbbf339a192b17c144e22
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2025-07-09 09:43:40 +0530

    new change

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 const allowedOrigins = [
