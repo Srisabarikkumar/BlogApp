0c87f59db2bc90b426333811bc3bb42db4be66ff
srisabarikkumar@gmail.com <srisabarikkumar@gmail.com>
2025-07-11 13:04:08 +0530

    new change

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,7 @@ const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
